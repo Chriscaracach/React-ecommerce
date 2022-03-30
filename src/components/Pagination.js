39184072825501d44f83047371e3a38d6aca2e6c
chriscaracach <christiancaracach@gmail.com>
2022-03-29 21:25:02 -0300

    pagination

---
+++ src/components/Pagination.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Pagination from "@mui/material/Pagination";
+
+const Paginator = () => {
+  return <Pagination count={10} color="primary" />;
+};
+
+export default Paginator;
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -11,35 +11,51 @@ import { Box } from "@mui/material";
 import { Typography } from "@mui/material";
 import ErrorIcon from "@mui/icons-material/Error";
 import MenuItem from "@mui/material/MenuItem";
+import { Pagination } from "@mui/material";
 
 const Shop = () => {
   const { products } = useSelector((state) => state.products);
   const [filteredProducts, setFilteredProducts] = useState(products);
+  const [page, setPage] = useState(1);
+  const [pageAmount, setPageAmount] = useState(1);
+  const [category, setCategory] = useState("");
+  const [searchError, setSearchError] = useState(false);
 
-  const map = filteredProducts.map((product) => {
-    return (
-      products.length && (
-        <Grid item key={product.id}>
-          <ProductCard
-            id={product.id}
-            title={product.title}
-            img={product.image}
-            price={product.price}
-            alt={product.title}
-            description={product.description}
-            rating={product.rating.rate}
-          ></ProductCard>
-        </Grid>
-      )
-    );
-  });
+  const map = filteredProducts
+    .slice((page - 1) * 3, (page - 1) * 3 + 3)
+    .map((product) => {
+      return (
+        products.length && (
+          <Grid item key={product.id}>
+            <ProductCard
+              id={product.id}
+              title={product.title}
+              img={product.image}
+              price={product.price}
+              alt={product.title}
+              description={product.description}
+              rating={product.rating.rate}
+            ></ProductCard>
+          </Grid>
+        )
+      );
+    });
 
   const handleSearch = (e) => {
+    setSearchError(false);
+    console.log("antes ");
+    console.log(filteredProducts);
     setFilteredProducts(
       products.filter((product) =>
         product.title.toLowerCase().includes(e.target.value.toLowerCase())
       )
     );
+    //! este setFilteredProducts no funciona bien, cuando llega el if, todavía hay cosas de antes, correr y revisar
+    console.log("despues ");
+    console.log(filteredProducts);
+    if (!filteredProducts) {
+      setSearchError(true);
+    }
   };
 
   const handleCategorySearch = (e) => {
@@ -49,11 +65,20 @@ const Shop = () => {
           product.category.toLowerCase() === e.target.value.toLowerCase()
       )
     );
+    setCategory(e.target.value);
+  };
+
+  const handlePagination = (event, value) => {
+    setPage(value);
   };
 
   useEffect(() => {
     setFilteredProducts(products);
   }, [products]);
+
+  useEffect(() => {
+    setPageAmount(Math.ceil(filteredProducts.length / 3));
+  }, [filteredProducts]);
 
   return (
     <Container sx={{ marginTop: "100px" }}>
@@ -76,8 +101,8 @@ const Shop = () => {
               id="outlined-select-currency"
               select
               label="Select"
-              // value={""}
               onChange={handleCategorySearch}
+              value={category}
               helperText="Filter by category"
             >
               <MenuItem value={"electronics"}>Electronics</MenuItem>
@@ -97,6 +122,20 @@ const Shop = () => {
           >
             {map}
           </Grid>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            m={4}
+          >
+            <Pagination
+              count={pageAmount}
+              page={page}
+              onChange={handlePagination}
+            />
+          </Box>
         </>
       ) : (
         <Box
@@ -110,7 +149,7 @@ const Shop = () => {
           <CircularProgress />
         </Box>
       )}
-      {!filteredProducts.length && (
+      {searchError && (
         <Box
           sx={{
             display: "flex",
